45d250922b26c608654e408805ade64b687e650d
AleCasa <alecasa06@gmail.com>
2024-10-14 18:21:56 +0200

    non funziona

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.sql.*;
+
 public class Main {
+
     public static void main(String[] args) throws SQLException {
+
+
+
         String url = "jdbc:mysql://localhost:3306/cucinaalpina";
         String user = "root";
         String password = "root";
@@ -10,7 +15,7 @@ public class Main {
         Connection conn = DriverManager.getConnection(url, user, password);
         System.out.println("Connesso con successo!");
         Statement stmt = conn.createStatement();
-        String query = "SELECT persona.cognome, persona.nome FROM persona";
+        String query = "SELECT * FROM persona";
         ResultSet rs = stmt.executeQuery(query);
 
         while (rs.next()) {
